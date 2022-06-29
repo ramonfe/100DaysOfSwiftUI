ef48a48d93db5543a7d92e8663dafc0a8b944440
ramonfe-mini <ramonfe@gmail.com>
2022-06-28 22:28:57 -0700

    finishing project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ramon Felix on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
