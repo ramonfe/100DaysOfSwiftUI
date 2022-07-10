7a91bd0ab369798f579ed17b9b8a264924d0e784
ramonfe-mini <ramonfe@gmail.com>
2022-07-10 16:15:33 -0700

    finishing project

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -21,26 +21,39 @@ struct ContentView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(missions){mission in
                         NavigationLink{
-                            Text("Detail view")
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label:{
                             VStack{
                                 Image(mission.image)
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
-                                
+                                    .padding()
                                 VStack{
-                                    Text(mission.displayName).font(.headline)
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
                     }
+                    .padding([.horizontal, .bottom])
                 }
             }
             .navigationTitle("MoonShot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
