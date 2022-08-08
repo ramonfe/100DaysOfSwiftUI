c9a9448d8441dd50b7aa8a1e482d401500dbb8dd
ramonfe-mini <ramonfe@gmail.com>
2022-08-07 21:00:02 -0700

    challenge done

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 150.0
     
     @State private var showingImagePicture = false
     @State private var inputImage:UIImage?
@@ -21,6 +22,7 @@ struct ContentView: View {
     let context = CIContext()
     
     @State private var showingFilterSheet = false
+    @State private var hasImage = false
     
     var body: some View {
         NavigationView{
@@ -46,6 +48,12 @@ struct ContentView: View {
                     Slider(value: $filterIntensity)
                         .onChange(of: filterIntensity){ _ in applyProcessing() }
                 }
+                HStack{
+                    Text("Radius")
+                    Slider(value: $filterRadius, in: 1...300)
+                        .onChange(of: filterRadius){_ in applyProcessing() }
+                    
+                }
                 .padding(.vertical)
                 
                 HStack{
@@ -54,6 +62,7 @@ struct ContentView: View {
                     }
                     Spacer()
                     Button("save", action: save)
+                        .disabled(hasImage==false)
                 }
             }
             .padding([.horizontal,.bottom])
@@ -63,22 +72,30 @@ struct ContentView: View {
                 ImagePicker(image: $inputImage)
             }
             .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
-                
-                Button("Cristallize"){setFilter(CIFilter.crystallize())}
-                Button("Edges"){setFilter(CIFilter.edges())}
-                Button("Gaussian Blur"){setFilter(CIFilter.gaussianBlur())}
-                Button("Pixellate"){setFilter(CIFilter.pixellate())}
-                Button("Sepia Tone"){setFilter(CIFilter.sepiaTone())}
-                Button("Unsharp Mask"){setFilter(CIFilter.unsharpMask())}
-                Button("Vignette"){setFilter(CIFilter.vignette())}
-                Button("Cancel", role: .cancel){}
+                Section{
+                    Button("Cristallize"){setFilter(CIFilter.crystallize())}
+                    Button("Edges"){setFilter(CIFilter.edges())}
+                    Button("Gaussian Blur"){setFilter(CIFilter.gaussianBlur())}
+                    Button("Pixellate"){setFilter(CIFilter.pixellate())}
+                    Button("Sepia Tone"){setFilter(CIFilter.sepiaTone())}
+                    Button("Unsharp Mask"){setFilter(CIFilter.unsharpMask())}
+                }
+                Section{
+                    Button("Vignette"){setFilter(CIFilter.vignette())}
+                    Button("Distortion"){setFilter(CIFilter.twirlDistortion())}
+                    Button("Comic"){setFilter(CIFilter.comicEffect())}
+                    Button("Bloom"){setFilter(CIFilter.bloom())}
+                    Button("Line Overlay"){setFilter(CIFilter.lineOverlay())}
+                    Button("Cancel", role: .cancel){}
+                }
             }
         }
     }
     
     func loadImage(){
         guard let inputImage = inputImage else { return }
-//        image = Image(uiImage: inputImage)
+        //        image = Image(uiImage: inputImage)
+        hasImage = true
         let beginImage = CIImage(image: inputImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProcessing()
@@ -86,7 +103,7 @@ struct ContentView: View {
     
     func save(){
         guard let processedImage = processedImage else {return}
-
+        
         let imageSaver = ImageSaver()
         
         imageSaver.successHandler = {
@@ -102,7 +119,7 @@ struct ContentView: View {
         let inputKeys = currentFilter.inputKeys
         
         if inputKeys.contains(kCIInputIntensityKey){currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)}
-        if inputKeys.contains(kCIInputRadiusKey){currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputRadiusKey){currentFilter.setValue(filterRadius, forKey: kCIInputRadiusKey)}
         if inputKeys.contains(kCIInputScaleKey){currentFilter.setValue(filterIntensity *  10, forKey: kCIInputScaleKey)}
         //currentFilter.intensity = Float(filterIntensity)
         
