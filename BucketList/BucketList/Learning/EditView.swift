63452a04b99186fcc6efc70ee674918b4a4173d1
ramonfe <ramonfe@gmail.com>
2022-08-09 16:33:46 -0700

    finish day 71

--- BucketList/BucketList/Learning/EditView.swift
+++ BucketList/BucketList/Learning/EditView.swift
@@ -38,8 +38,8 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ")
-                            + Text("Page description here")
-                                .italic()
+                            + Text(page.desciption )
+                                 .italic()
                         }
                     case .failed:
                         Text("Please tray again later.")
@@ -81,7 +81,7 @@ struct EditView: View {
         do{
             let (data,_) = try await URLSession.shared.data(from: url)
             let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted{ $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded            
         }catch{
             loadingState = .failed
