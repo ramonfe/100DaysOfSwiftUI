2cf081c93def079621d6343d251b42ddb4d62d4f
ramonfe <ramonfe@gmail.com>
2022-08-10 12:31:10 -0700

    finishing day 72 -locking our ui behind face id

--- BucketList/BucketList/Learning/UserLocationView-ViewModel.swift
+++ BucketList/BucketList/Learning/UserLocationView-ViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 
@@ -13,8 +14,8 @@ extension UserLocationView{
     @MainActor class ViewModel: ObservableObject{
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
         @Published private(set)  var locations: [Location]
-        
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
         
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
         
@@ -40,6 +41,28 @@ extension UserLocationView{
             let newLocation  = Location(id: UUID(), name: "New Location", description: "", latitud: mapRegion.center.latitude, longitud: mapRegion.center.longitude)
             locations.append(newLocation)
             save()
+        }
+        
+        func authenticate(){
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+                let reason = "Please authenticate yourself to unlock you places."
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    if success{
+                        Task {
+                            @MainActor in
+                            self.isUnlocked = true
+                        }
+                    }else{
+                        //error
+                    }
+                }
+            }else{
+                //other error
+            }
         }
         
         func update(location: Location){
