0c707d97250d1eaeed5e944d1f003f6f1da1f553
ramonfe-mini <ramonfe@gmail.com>
2022-08-30 20:37:02 -0700

    finishing pFlashzilla D90

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -17,7 +17,7 @@ extension View{
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
-    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    @State private var cards = [Card]()
     @State private var showingEditScreen = false
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -131,6 +131,8 @@ struct ContentView: View {
                 isActive = false
             }
         }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards, content: EditCards.init)
+        .onAppear(perform: resetCards)
     }
     func removeCard(at index: Int){
         guard index >= 0 else { return }
@@ -140,10 +142,17 @@ struct ContentView: View {
         }
     }
     
-    func resetCards(){
-        cards = Array(repeating: Card.example, count: 10)
+    func loadData(){
+        if let data = UserDefaults.standard.data(forKey: "Cards"){
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data){
+                cards = decoded
+            }
+        }
+    }
+    func resetCards(){       
         timeRemaining = 100
         isActive = true
+        loadData()
     }
 }
 
